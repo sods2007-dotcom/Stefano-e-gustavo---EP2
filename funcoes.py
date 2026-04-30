36e87a73946f16ea7a3c5323da84cfb317f46a4a
Stefano <sods2007@gmail.com>
2026-04-30 10:28:56 -0300

    exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,15 @@ def rolar_dados(dados):
     for i in range(dados):
         resultado.append(random.randint(1, 6))
     return resultado
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    valor = dados_rolados[dado_para_guardar]
+    
+    dados_no_estoque.append(valor)
+    
+    nova_lista = []
+    for i in range(len(dados_rolados)):
+        if i != dado_para_guardar:
+            nova_lista.append(dados_rolados[i])
+    
+    return [nova_lista, dados_no_estoque]
