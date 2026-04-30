9afbd1088cd4c23f91187dd620427f36d62580d7
gustavo-neymar <gustavopq@al.insper.edu.br>
2026-04-30 17:02:01 -0300

    Exercicio 4

--- funcoes.py
+++ funcoes.py
@@ -27,3 +27,13 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
             nova_lista.append(dados_no_estoque[i])
     
     return [dados_rolados, nova_lista]
+
+def calcula_pontos_regra_simples(dados):
+    pontos = {}
+    for categoria in range(1, 7):
+        soma = 0
+        for dado in dados:
+            if dado == categoria:
+                soma += dado
+        pontos[categoria] = soma
+    return pontos
